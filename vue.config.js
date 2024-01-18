3efb7fb7a83856fbccbcfc5a0b50b61ba42bb0af
menethyl <stapps90@gmail.com>
2024-01-18 17:53:35 +0300

    changed build

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/port-heaven/" : "/",
-  };
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/port-heaven/'
+    : '/'
+}
