f3646a84231f6acf171dd60e470d4ff6eb936cd9
d1eruki <stapps90@gmail.com>
2024-09-16 01:39:17 +0300

    many fixes, incld " + classes

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-$('[data-open-block]').on('click', function() {
-	$('[data-open-block]').children().removeClass('active');
-	$('[data-content]').removeClass('is-active');
-	$(this).children().addClass('active');
-	$(`[data-content='${$(this).data('open-block')}'`).addClass('is-active');
+$("[data-open-block]").on("click", function() {
+	$("[data-open-block]").children().removeClass("active");
+	$("[data-content]").removeClass("is-active");
+	$(this).children().addClass("active");
+	$(`[data-content="${$(this).data("open-block")}"`).addClass("is-active");
 });
 
-jQuery('#qrc').qrcode({
-	render	: 'table',
-	text	: 'https://t.me/d1eruki'
+jQuery(".qrc").qrcode({
+	render	: "table",
+	text	: "https://t.me/d1eruki"
 });
 
 $(document).ready(function(){
 	$(".cardBox").hover(function(){
-		document.body.css('background-color','grey');
+		document.body.css("background-color","grey");
 	});
 	$(".cardBox").mouseleave(function(){
-		document.body.css('background-color','white');
+		document.body.css("background-color","white");
 	});
 });
 
 function cheat() {
-	var cheatCode = document.getElementById('cheat').value;
-	var audio = new Audio('sources/sounds/phonk.wav');
+	var cheatCode = document.getElementById("cheat").value;
+	var audio = new Audio("sources/sounds/phonk.wav");
 	audio.loop = true;
-	if (cheatCode == 'phonk') {
+	if (cheatCode == "phonk") {
 		audio.play();
 	}
-	if (cheatCode == 'phonkstop') {
+	if (cheatCode == "phonkstop") {
 		audio.stop();
 	}
-	if (cheatCode == 'cheats') {
+	if (cheatCode == "cheats") {
 		header.createElement("h1");
 	}
 }
