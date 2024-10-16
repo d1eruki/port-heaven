c9c2ff1b2bf16a6d8b257f931539f40acdecda2f
d1eruki <stapps90@gmail.com>
2024-10-17 01:08:43 +0300

    fixes after enabling tw p1

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,42 +1,32 @@
 $("[data-open-block]").on("click", function() {
-	$("[data-open-block]").children().removeClass("active");
-	$("[data-content]").removeClass("is-active");
-	$(this).children().addClass("active");
-	$(`[data-content="${$(this).data("open-block")}"`).addClass("is-active");
+	var $this = $(this);
+	var target = $this.data("open-block");
+
+	// Удаляем активные классы только один раз
+	$("[data-open-block].active").removeClass("active");
+	$("[data-content].is-active").removeClass("is-active");
+
+	// Добавляем активные классы
+	$this.addClass("active");
+	$(`[data-content="${target}"]`).addClass("is-active");
 });
+
+function cheat() {
+	var cheatCode = $("#cheat").val(); // Получаем значение input с id "cheat"
+	var audio = new Audio("src/sounds/phonk.wav");
+	audio.loop = true;
+
+	if (cheatCode === "phonk") {
+		audio.play();
+	} else if (cheatCode === "phonkstop") {
+		audio.pause();
+		audio.currentTime = 0; // Устанавливаем время воспроизведения на 0, если нужно остановить
+	} else if (cheatCode === "cheats") {
+		$("header").append("<h1>Cheats Activated</h1>");
+	}
+}
 
 jQuery(".qrc").qrcode({
 	render	: "table",
 	text	: "https://t.me/d1eruki"
-});
-
-$(document).ready(function(){
-	$(".cardBox").hover(function(){
-		document.body.css("background-color","grey");
-	});
-	$(".cardBox").mouseleave(function(){
-		document.body.css("background-color","white");
-	});
-});
-
-function cheat() {
-	var cheatCode = document.getElementById("cheat").value;
-	var audio = new Audio("src/sounds/phonk.wav");
-	audio.loop = true;
-	if (cheatCode == "phonk") {
-		audio.play();
-	}
-	if (cheatCode == "phonkstop") {
-		audio.stop();
-	}
-	if (cheatCode == "cheats") {
-		header.createElement("h1");
-	}
-}
-
-$(document).keypress(function(e){
-	if (e.which == 13){
-		$(".cheatEnter").click();
-		$(".cheatEnter").reset();
-	}
 });
