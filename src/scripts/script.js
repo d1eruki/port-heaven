7ffcd7a80305eccd43fa260abe42dc393e9a35cb
d1eruki <stapps90@gmail.com>
2024-10-24 01:23:23 +0300

    changes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,7 +18,9 @@ $("[data-open-block]").on("click", function () {
     }
 });
 
-// Находим элемент с классом kun
+/////////////////////////////////////////////////////
+
+// Находим элемент с классом сhess
 const сhess = document.querySelector('.сhess');
 
 // Создаем пустую строку для хранения всех div
@@ -27,7 +29,7 @@ let htmlContent = '';
 // Переменная для отслеживания текущего цвета
 let currentColor = 'white';
 
-// Генерируем 290 div-ов
+// Генерируем 324 div-ов
 for (let i = 0; i < 324; i++) {
     // Каждые 18 строк нужно повторить предыдущий цвет
     if (i > 0 && i % 18 === 0) {
@@ -47,8 +49,37 @@ for (let i = 0; i < 324; i++) {
 // Вставляем все div-ы за один раз
 сhess.innerHTML = htmlContent;
 
+/////////////////////////////////////////////////////
 
+// Получаем элементы
+const header = document.querySelector('header');
+const footer = document.querySelector('footer');
+const heightTarget = document.getElementById('height-target');
 
+// Функция для установки высоты
+const setHeight = () => {
+    if (!header || !footer) return; // Проверка на существование элементов
+
+    const headerHeight = header.offsetHeight;
+
+    // Устанавливаем высоту футера равной высоте хэдера
+    footer.style.height = `${headerHeight}px`;
+
+    // Обновляем высоту height-target
+    heightTarget.style.height = `calc(100vh - ${headerHeight}px - ${headerHeight}px)`;
+};
+
+// Устанавливаем высоту при загрузке страницы
+setHeight();
+
+// Обновляем высоту при изменении размера окна
+let resizeTimeout;
+window.addEventListener('resize', () => {
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(setHeight, 100); // 100ms задержка
+});
+
+/////////////////////////////////////////////////////
 
 jQuery(".qrc").qrcode({
     render: "table",
