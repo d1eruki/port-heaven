d807bed51cfdbbb5665136ed427efb3bf488f35f
d1eruki <stapps90@gmail.com>
2024-10-23 22:30:51 +0300

    changes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -18,6 +18,38 @@ $("[data-open-block]").on("click", function () {
     }
 });
 
+// Находим элемент с классом kun
+const сhess = document.querySelector('.сhess');
+
+// Создаем пустую строку для хранения всех div
+let htmlContent = '';
+
+// Переменная для отслеживания текущего цвета
+let currentColor = 'white';
+
+// Генерируем 290 div-ов
+for (let i = 0; i < 324; i++) {
+    // Каждые 18 строк нужно повторить предыдущий цвет
+    if (i > 0 && i % 18 === 0) {
+        // Повторяем предыдущий цвет
+        htmlContent += `<div class="${currentColor}"></div>`;
+    } else {
+        // Чередуем цвета white и black
+        if (currentColor === 'white') {
+            currentColor = 'black';
+        } else {
+            currentColor = 'white';
+        }
+        htmlContent += `<div class="${currentColor}"></div>`;
+    }
+}
+
+// Вставляем все div-ы за один раз
+сhess.innerHTML = htmlContent;
+
+
+
+
 jQuery(".qrc").qrcode({
     render: "table",
     text: "https://t.me/d1eruki"
