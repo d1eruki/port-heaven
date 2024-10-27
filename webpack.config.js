db8a609db56e5e35a49b343d3c895ce9dfe775d6
d1eruki <stapps90@gmail.com>
2024-10-27 05:24:09 +0300

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/scripts/scripts.js', // Входной файл (твой основной файл JS)
     output: {
+        path: path.resolve(__dirname, 'docs'), // Папка для выхода, например, "docs"
         filename: 'scripts.js', // Имя выходного файла
-        path: path.resolve(__dirname, 'docs'), // Папка для выхода, например, "dist"
+        publicPath: '/', // этот путь будет добавлен к каждому ресурсу
+        clean: true, // очищает выходную папку перед сборкой
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpe?g|gif|svg|ico)$/i, // изображения
+                type: 'asset/resource', // автоматически копирует в выходную папку и генерирует путь
+                generator: {
+                    filename: 'images/[name][ext]', // настройка папки для изображений
+                },
+            },
+            {
+                test: /\.css$/i,
+                exclude: /exclude-this-file\.css$/, // Укажите файл или паттерн, который нужно исключить
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
     },
     plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html', // исходный HTML
+            filename: 'index.html',
+        }),
         new CopyWebpackPlugin({
             patterns: [
-                {
-                    from: path.resolve(__dirname, 'src'), // копируем всю папку src
-                    to: path.resolve(__dirname, 'docs'), // в папку docs
-                    globOptions: {
-                        ignore: ['**/*.js', '**/*.scss'], // исключаем index.html и js (если они собираются)
-                    },
-                },
+                { from: 'src/images', to: 'images' }, // Копирование изображений
             ],
         }),
     ],
