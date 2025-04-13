31df4867ecdbb0b81cca39f68b27180eca183ed2
d1eruki <stapps90@gmail.com>
2025-04-13 19:48:22 +0300

    make poster card

--- src/script.js
+++ src/script.js
@@ -1,16 +1,27 @@
 import './style.scss';
 import './js/menu-swap';
 import './js/vue';
-import { setHeight } from './js/header-footer-height';
-import { updateProgressBar } from './js/progress-bar';
-import { generateChessboard } from './js/chessboard';
-import { updateHeaderOnScroll } from './js/scroll-header';
+import {setHeight} from './js/header-footer-height';
+import {updateProgressBar} from './js/progress-bar';
+import {generateChessboard} from './js/chessboard';
+import {updateHeaderOnScroll} from './js/scroll-header';
+import VanillaTilt from 'vanilla-tilt';
 
 document.addEventListener('DOMContentLoaded', () => {
     setHeight();
+
+    const pok = document.querySelector(".pok");
+    if (pok) {
+        VanillaTilt.init(pok, {
+            speed: 1000,
+            transition: true,
+            easing: "cubic-bezier(0.23, 1, 0.32, 1)",
+            glare: true,
+            "max-glare": 0.35
+        });
+    }
 });
 
 window.onscroll = updateProgressBar;
-
 generateChessboard();
 updateHeaderOnScroll();
