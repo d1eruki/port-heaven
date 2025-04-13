9432f8c7af1fecab4a021eaa1452c557f98af3b8
d1eruki <stapps90@gmail.com>
2025-04-13 23:46:22 +0300

    make changes for poster

--- src/js/card-rotate.js
+++ src/js/card-rotate.js
@@ -1,9 +1,9 @@
 import VanillaTilt from 'vanilla-tilt';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const pok = document.querySelector(".pok");
-    if (pok) {
-        VanillaTilt.init(pok, {
+    const card = document.querySelector(".card");
+    if (card) {
+        VanillaTilt.init(card, {
             speed: 1000,
             easing: "cubic-bezier(0.23, 1, 0.32, 1)",
             transition: true,
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', () => {
             "max-glare": 0.35
         });
 
-        pok.addEventListener('mouseleave', () => {
-            pok.style.transition = 'transform 0.8s cubic-bezier(0.23, 1, 0.32, 1)';
-            pok.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg)';
+        card.addEventListener('mouseleave', () => {
+            card.style.transition = 'transform 0.8s cubic-bezier(0.23, 1, 0.32, 1)';
+            card.style.transform = 'perspective(800px) rotateX(0deg) rotateY(0deg)';
         });
     }
 });
--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 import './style.scss';
 import './js/menu-swap';
 import './js/vue';
+import './js/card-rotate';
 import {setHeight} from './js/header-footer-height';
 import {updateProgressBar} from './js/progress-bar';
 import {generateChessboard} from './js/chessboard';
 import {updateHeaderOnScroll} from './js/scroll-header';
-import './js/card-rotate'; // Импорт нового файла
 
 document.addEventListener('DOMContentLoaded', () => {
     setHeight();
