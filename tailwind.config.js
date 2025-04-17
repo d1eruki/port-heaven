cc2d2f8cc380c53923231525bcf5e7dd205f7862
d1eruki <stapps90@gmail.com>
2025-04-17 21:33:25 +0300

    some changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       height: {
         "100vh-64": "calc(100vh - 64px)",
       },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1440px', // максимальная ширина для 2xl
+        },
+      },
     },
   },
   plugins: [],
