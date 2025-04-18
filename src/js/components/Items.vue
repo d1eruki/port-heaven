78d12bdc14897099eed9ced3e19cac95b659d879
d1eruki <stapps90@gmail.com>
2025-04-18 23:24:24 +0300

    new grid for stack

--- src/js/components/Items.vue
+++ src/js/components/Items.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="item pointer-events-none flex flex-col rounded-xl p-4 md:pointer-events-auto">
     <div class="mb-4 flex items-center justify-between gap-1">
-      <h2>{{ itemName }}</h2>
+      <h4>{{ itemName }}</h4>
       <img class="rounded-xl drop-shadow-none" :src="itemImage" :alt="itemImgAlt" />
     </div>
     <p>{{ itemDesc }}</p>
