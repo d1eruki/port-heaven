bdd18b06014e644fe60dba06d41843e26be4706b
d1eruki <stapps90@gmail.com>
2025-04-18 16:46:02 +0300

    remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -41,16 +41,6 @@ module.exports = {
         loader: "vue-loader",
       },
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
         test: /\.scss$/i,
         use: [
           MiniCssExtractPlugin.loader,
