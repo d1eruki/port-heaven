163e31454f82e7a35d7c1481a68d1d2ae41e25df
d1eruki <stapps90@gmail.com>
2025-04-23 01:32:54 +0300

    blank for parallax.js

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ import "./js/vue";
 import "./js/change-logo";
 import "./js/menu-swap";
 import "./js/scroll-to-section";
+import "./js/parallax";
 import { updateProgressBar } from "./js/progress-bar";
 
 window.onscroll = updateProgressBar;
