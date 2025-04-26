72f5ec6bd0978c7173a1ef3890605613a5c23108
d1eruki <stapps90@gmail.com>
2025-04-26 15:09:41 +0300

    figma thumbnails

--- src/js/components/Items.vue
+++ src/js/components/Items.vue
@@ -1,35 +1,40 @@
 <template>
-  <a :href="youtubeUrl" target="_blank" class="item flex flex-col rounded-xl aspect-video relative">
-    <h4 class="z-5 p-4 hidden">{{ itemName }}</h4>
-    <img class="rounded-xl drop-shadow-none absolute grayscale hover:grayscale-0" :src="thumbnailUrl" :alt="itemImgAlt" />
+  <a :href="itemUrl" target="_blank" class="item relative flex aspect-video flex-col rounded-xl">
+    <h4 class="z-10 p-4">{{ itemName }}</h4>
+    <img class="absolute top-0 left-0 h-full w-full rounded-xl object-cover drop-shadow-none grayscale hover:grayscale-0" :src="thumbnailUrl" :alt="itemImgAlt" />
   </a>
 </template>
 
 <script setup>
-import { computed } from 'vue';
+import { computed } from "vue";
 
 const props = defineProps({
   itemName: {
     type: String,
     required: true,
-    default: 'Default Title',
+    default: "Default Title",
   },
-  youtubeUrl: {
+  itemUrl: {
     type: String,
     required: true,
-    default: 'https://www.youtube.com/',
+    default: "https://www.youtube.com/",
   },
   itemImgAlt: {
     type: String,
-    required: true,
-    default: 'YouTube Video Thumbnail',
+    required: false,
+    default: "",
   },
 });
 
 const thumbnailUrl = computed(() => {
-  const regex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-  const match = props.youtubeUrl.match(regex);
-  const videoId = match ? match[1] : null;
-  return videoId ? `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg` : 'assets/images/default.svg';
+  const youtubeRegex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+  const youtubeMatch = props.itemUrl.match(youtubeRegex);
+  if (youtubeMatch) {
+    const videoId = youtubeMatch[1];
+    return `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+  }
+
+  const figmaRegex = /figma\.com\/(?:file|design)\/([a-zA-Z0-9_-]{22})/;
+  const figmaMatch = props.itemUrl.match(figmaRegex);
 });
 </script>
--- src/js/vue.js
+++ src/js/vue.js
@@ -1,14 +1,6 @@
 import { createApp } from "vue";
 import Items from "./components/Items.vue";
 
-const designApp = createApp({});
-designApp.component("design-item", Items);
-designApp.mount("#design-app");
-
-const webApp = createApp({});
-webApp.component("web-item", Items);
-webApp.mount("#web-app");
-
-const videoApp = createApp({});
-videoApp.component("video-item", Items);
-videoApp.mount("#video-app");
+const itemApp = createApp({});
+itemApp.component("item", Items);
+itemApp.mount("#item-app");
