41743db39e89eed39e0a903d4f7197adb936be68
d1eruki <stapps90@gmail.com>
2025-04-27 16:33:04 +0300

    revisual item

--- src/js/components/items.vue
+++ src/js/components/items.vue
@@ -1,7 +1,7 @@
 <template>
-  <a :href="itemUrl" target="_blank" class="item relative flex aspect-video flex-col rounded-xl">
+  <a :href="itemUrl" target="_blank" :class="['item relative flex aspect-video flex-col rounded-xl', showThumbnail ? '' : 'border']">
     <h4 v-if="!isYoutube" class="z-10 p-4">{{ itemName }}</h4>
-    <img class="absolute top-0 left-0 h-full w-full rounded-xl object-cover drop-shadow-none grayscale hover:grayscale-0" :src="thumbnailUrl" :alt="itemImgAlt" />
+    <img v-if="showThumbnail && thumbnailUrl" class="absolute top-0 left-0 h-full w-full rounded-xl object-cover drop-shadow-none grayscale hover:grayscale-0" :src="thumbnailUrl" :alt="itemImgAlt" @error="handleImageError" />
   </a>
 </template>
 
@@ -26,29 +26,45 @@ const props = defineProps({
   },
 });
 
-const thumbnailUrl = ref("");
+const thumbnailUrl = ref(null);
 const isYoutube = ref(false);
+const showThumbnail = ref(false);
+
+const handleImageError = () => {
+  showThumbnail.value = false;
+};
 
 watch(
   () => props.itemUrl,
   async (newUrl) => {
+    thumbnailUrl.value = null;
+    showThumbnail.value = false;
+
     const youtubeRegex = /(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const youtubeMatch = newUrl.match(youtubeRegex);
     if (youtubeMatch) {
       const videoId = youtubeMatch[1];
       thumbnailUrl.value = `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
-      isYoutube.value = true; // Устанавливаем флаг, что это YouTube ссылка
+      showThumbnail.value = true;
+      isYoutube.value = true;
       return;
     } else {
-      isYoutube.value = false; // Если не YouTube, скрываем флаг
+      isYoutube.value = false;
     }
 
     try {
       const response = await fetch(`http://localhost:3000/meta-image?url=${encodeURIComponent(newUrl)}`);
       const data = await response.json();
-      thumbnailUrl.value = data.image || "";
+      if (data.image) {
+        thumbnailUrl.value = data.image;
+        showThumbnail.value = true;
+      } else {
+        thumbnailUrl.value = null;
+        showThumbnail.value = false;
+      }
     } catch (error) {
-      thumbnailUrl.value = "";
+      thumbnailUrl.value = null;
+      showThumbnail.value = false;
     }
   },
   { immediate: true },
