27f779ce9a6de78b34842cca213d524c519d3192
d1eruki <stapps90@gmail.com>
2025-06-05 00:20:02 +0300

    about section

--- src/js/vue.js
+++ src/js/vue.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import Items from "./components/items.vue";
 import Icons from "./components/section-icons.vue";
+import Bullets from "./components/bullets.vue";
 
 const app = createApp({});
 app.component("item", Items);
 app.component("icon", Icons);
+app.component("bullet", Bullets);
 app.mount("#app");
--- src/script.js
+++ src/script.js
@@ -5,6 +5,7 @@ import "./js/menu-swap";
 import "./js/scroll-to-section";
 import "./js/scroll-to-top";
 import "./js/parallax";
+import "./js/draw-elements";
 import { updateProgressBar } from "./js/progress-bar";
 
 window.onscroll = updateProgressBar;
