58421b5b822415ed1e72ff51a7892028d8b4f27f
d1eruki <stapps90@gmail.com>
2025-06-05 23:13:35 +0300

    small fixes

--- src/js/components/key-features.vue
+++ src/js/components/key-features.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="inline-block w-0.5 self-stretch bg-(--grey-dark)"></div>
+  <div class="inline-block w-0.5 self-stretch dark:bg-(--grey-dark) light:bg-(--grey-light)"></div>
   <div class="flex flex-1 flex-col gap-2 lg:gap-10">
-    <span class="biryani numbers !text-[var(--grey-dark)]">{{ number }}</span>
+    <span class="biryani numbers dark:text-(--grey-dark) light:text-(--grey-light)">{{ number }}</span>
     <h3>{{ title }}</h3>
     <p>{{ text }}</p>
   </div>
