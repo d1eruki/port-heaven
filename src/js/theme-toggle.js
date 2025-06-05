928c1b3e1aab71b3d19401cecae3f155cdd92ba4
d1eruki <stapps90@gmail.com>
2025-06-05 19:51:53 +0300

    theme toggler

---
+++ src/js/theme-toggle.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const root = document.documentElement;
+  // Установим начальную тему, если не задана
+  if (!root.getAttribute("data-theme")) {
+    root.setAttribute("data-theme", "light");
+  }
+
+  document.getElementById("theme-toggle").onclick = () => {
+    const currentTheme = root.getAttribute("data-theme");
+    root.setAttribute("data-theme", currentTheme === "dark" ? "light" : "dark");
+  };
+});
--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
 import "./style.scss";
 import "./js/vue";
-import "./js/change-logo";
 import "./js/menu-swap";
 import "./js/scroll-to-section";
 import "./js/scroll-to-top";
 import "./js/parallax";
 import "./js/draw-elements";
+import "./js/theme-toggle";
 import { updateProgressBar } from "./js/progress-bar";
 
 window.onscroll = updateProgressBar;
