52536fdbebf5825ccc586bbcceb5976ae172f7fb
d1eruki <stapps90@gmail.com>
2025-06-05 23:13:01 +0300

    remove parallax.js

--- src/script.js
+++ src/script.js
@@ -3,7 +3,6 @@ import "./js/vue";
 import "./js/menu-swap";
 import "./js/scroll-to-section";
 import "./js/scroll-to-top";
-import "./js/parallax";
 import "./js/draw-elements";
 import "./js/theme-toggle";
 import { updateProgressBar } from "./js/progress-bar";
