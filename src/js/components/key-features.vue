331ed1af73ab8b68aa66190de75597bd8704c130
d1eruki <stapps90@gmail.com>
2025-06-05 21:08:04 +0300

    vue for key features

---
+++ src/js/components/key-features.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="inline-block w-0.5 self-stretch bg-(--grey-light)"></div>
+  <div class="flex flex-1 flex-col gap-2 lg:gap-10">
+    <span class="biryani numbers !text-[var(--grey-light)]">{{ number }}</span>
+    <div>
+      <svg viewBox="-150 -150 300 300" style="width: 30px; height: 30px">
+        <polygon class="star" points=""></polygon>
+      </svg>
+      <h3>{{ title }}</h3>
+    </div>
+    <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  number: {
+    type: String,
+    required: true,
+    default: "00",
+  },
+  title: {
+    type: String,
+    required: true,
+    default: "default title",
+  },
+  text: {
+    type: String,
+    required: true,
+    default: "default text",
+  },
+});
+</script>
--- src/js/vue.js
+++ src/js/vue.js
@@ -2,9 +2,11 @@ import { createApp } from "vue";
 import Items from "./components/items.vue";
 import Icons from "./components/section-icons.vue";
 import Bullets from "./components/bullets.vue";
+import Features from "./components/key-features.vue";
 
 const app = createApp({});
 app.component("item", Items);
 app.component("icon", Icons);
 app.component("bullet", Bullets);
+app.component("futures", Features);
 app.mount("#app");
