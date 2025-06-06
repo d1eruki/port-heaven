564690aff2fa64cd615f4b160598e3e718617e36
d1eruki <stapps90@gmail.com>
2025-06-06 21:37:25 +0300

    add dev banner

--- src/script.js
+++ src/script.js
@@ -7,6 +7,7 @@ import "./js/draw-elements";
 import "./js/theme-toggle";
 import "./js/locale-toggler";
 import "./js/banner-offset";
+import "./js/marquee";
 import { updateProgressBar } from "./js/progress-bar";
 
 window.onscroll = updateProgressBar;
