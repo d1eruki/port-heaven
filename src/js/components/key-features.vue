2a79f67a47aa81577a2cc8c02a60d63226dfbfcd
d1eruki <stapps90@gmail.com>
2025-06-08 14:03:25 +0300

    fixes width container

--- src/js/components/key-features.vue
+++ src/js/components/key-features.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="light:bg-(--grey) inline-block w-0.5 self-stretch dark:bg-(--grey-dark)"></div>
+  <div class="light:bg-(--grey) inline-block w-[1px] self-stretch dark:bg-(--grey-dark)"></div>
   <div class="flex flex-1 flex-col gap-2 lg:gap-10">
     <span class="biryani numbers light:text-(--grey) dark:text-(--grey-dark)">{{ number }}</span>
     <h3>{{ title }}</h3>
