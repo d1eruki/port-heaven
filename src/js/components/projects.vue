eae4321ac561600185ece825feebf6a9cb87dc96
d1eruki <stapps90@gmail.com>
2025-10-04 00:21:06 +0300

    prettier

--- src/js/components/projects.vue
+++ src/js/components/projects.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="light:bg-[var(--black)] overflow-hidden
-
-
-
- item project absolute col-span-1 flex h-full w-full flex-col rounded-4xl border border-solid border-white will-change-transform lg:h-full">
+  <div class="light:bg-[var(--black)] item project absolute col-span-1 flex h-full w-full flex-col overflow-hidden rounded-4xl border border-solid border-white will-change-transform lg:h-full">
     <div class="flex h-full flex-col justify-between gap-5">
-      <h3 class="lg:w-[80%] p-5">{{ projectName }}</h3>
+      <h3 class="p-5 lg:w-[80%]">{{ projectName }}</h3>
       <p class="p-5">{{ projectDescription }}</p>
       <div class="flex h-fit w-full justify-end">
         <a v-for="(link, index) in projectLinks" :key="index" class="active custom-button flex w-full items-center justify-center px-5 py-5 lg:px-10" :href="link.url" target="_blank">
