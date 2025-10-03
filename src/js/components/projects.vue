d2d3658124c0a7ba46625910087ea65cc72ae25e
d1eruki <stapps90@gmail.com>
2025-10-04 02:40:07 +0300

    prettify

--- src/js/components/projects.vue
+++ src/js/components/projects.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="item light:bg-[var(--white)] project absolute col-span-1 flex w-full flex-col gap-5 rounded-4xl border border-solid border-black will-change-transform">
     <div class="flex h-full flex-col justify-between gap-5 p-15">
-      <div class="flex gap-5 flex-col">
-        <h3 class="lg:w-[80%] light:text-[color:var(--black)]">{{ projectName }}</h3>
+      <div class="flex flex-col gap-5">
+        <h3 class="light:text-[color:var(--black)] lg:w-[80%]">{{ projectName }}</h3>
         <p class="light:text-[color:var(--grey)]">{{ projectDescription }}</p>
       </div>
       <p class="light:text-[color:var(--black)]">{{ projectText }}</p>
       <div class="flex flex-wrap gap-2">
-        <div v-for="(tag, index) in projectTags" :key="index" class="active custom-button flex w-fit">
-          <p class="w-fit light:text-[color:var(--black)] border border-solid border-black rounded-4xl px-5">{{ getTagName(tag) }}</p>
+        <div v-for="(tag, index) in projectTags" :key="index" class="flex">
+          <p class="light:text-[color:var(--black)] rounded-4xl border border-solid border-black px-5">{{ getTagName(tag) }}</p>
         </div>
       </div>
       <div class="flex">
-        <a v-for="(link, index) in projectLinks" :key="index" class="active custom-button flex w-full" :href="link.url" target="_blank">
-          <p class="w-fit">{{ link.name }}</p>
+        <a v-for="(link, index) in projectLinks" :key="index" class="active flex w-full" :href="link.url" target="_blank">
+          <p>{{ link.name }}</p>
         </a>
       </div>
     </div>
@@ -43,10 +43,10 @@ const props = defineProps({
 
 function getTagName(tag) {
   if (tag == null) return "";
-  if (typeof tag === 'string' || typeof tag === 'number') return String(tag);
-  if (typeof tag === 'object') {
-    return tag.name || tag.label || tag.title || '';
+  if (typeof tag === "string" || typeof tag === "number") return String(tag);
+  if (typeof tag === "object") {
+    return tag.name || tag.label || tag.title || "";
   }
-  return '';
+  return "";
 }
 </script>
