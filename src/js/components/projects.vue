9c3dfe63218fd45fedf8340cfefac1c1453920be
d1eruki <stapps90@gmail.com>
2025-10-04 01:02:35 +0300

    change some visual

--- src/js/components/projects.vue
+++ src/js/components/projects.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="light:bg-[var(--black)] item project absolute col-span-1 flex h-full w-full flex-col overflow-hidden rounded-4xl border border-solid border-white will-change-transform lg:h-full">
-    <div class="flex h-full flex-col justify-between gap-5">
-      <h3 class="p-5 lg:w-[80%]">{{ projectName }}</h3>
-      <p class="p-5">{{ projectDescription }}</p>
-      <div class="flex h-fit w-full justify-end">
-        <a v-for="(link, index) in projectLinks" :key="index" class="active custom-button flex w-full items-center justify-center px-5 py-5 lg:px-10" :href="link.url" target="_blank">
+  <div class="item light:bg-[var(--white)] project absolute col-span-1 flex w-full flex-col gap-5 rounded-t-4xl will-change-transform border border-solid border-black">
+    <div class="flex flex-col justify-between gap-5 h-full p-5">
+      <h3 class="lg:w-[80%]">{{ projectName }}</h3>
+      <p class="">{{ projectDescription }}</p>
+      <div class="flex h-fit w-full justify-end gap-5">
+        <a v-for="(link, index) in projectLinks" :key="index" class="active custom-button flex w-full items-center justify-center rounded-4xl px-5 py-5 lg:px-10" :href="link.url" target="_blank">
           <p>{{ link.name }}</p>
         </a>
       </div>
