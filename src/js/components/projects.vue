62f3cdb17d8ba55348d6c9ab1c38672f224b44ce
d1eruki <stapps90@gmail.com>
2025-10-04 00:19:56 +0300

    gsap ready

--- src/js/components/projects.vue
+++ src/js/components/projects.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="light:bg-[var(--black)] item project absolute col-span-1 flex h-full w-full flex-col rounded-4xl border border-solid border-white will-change-transform lg:h-full">
-    <div class="flex h-full flex-col justify-between gap-5 p-5">
-      <h3>{{ projectName }}</h3>
-      <p>{{ projectDescription }}</p>
+  <div class="light:bg-[var(--black)] overflow-hidden
+
+
+
+ item project absolute col-span-1 flex h-full w-full flex-col rounded-4xl border border-solid border-white will-change-transform lg:h-full">
+    <div class="flex h-full flex-col justify-between gap-5">
+      <h3 class="lg:w-[80%] p-5">{{ projectName }}</h3>
+      <p class="p-5">{{ projectDescription }}</p>
       <div class="flex h-fit w-full justify-end">
         <a v-for="(link, index) in projectLinks" :key="index" class="active custom-button flex w-full items-center justify-center px-5 py-5 lg:px-10" :href="link.url" target="_blank">
           <p>{{ link.name }}</p>
