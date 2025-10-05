885b42fdd8764701b4ebdd6df858fa321f966dce
d1eruki <stapps90@gmail.com>
2025-10-06 01:48:46 +0300

    adding tag icons

--- src/js/components/designs.vue
+++ src/js/components/designs.vue
@@ -1,16 +1,23 @@
 <template>
-  <a :href="designUrl" target="_blank" class="group light:bg-[var(--black)] design active relative z-10 grid     justify-center gap-5 rounded-4xl p-10 hover:z-100 h-125  lg:items-center">
-    <h4 class="light:text-[color:var(--white)]">{{ designName }}</h4>
-    <p class="lg:absolute right-10 bottom-10 left-10 block self-end lg:hidden lg:group-hover:block">{{ designDesc }}</p>
-    <div class="lg:absolute right-10 bottom-10 left-10 flex flex-wrap gap-2 self-end lg:flex lg:group-hover:hidden">
-      <div v-for="(tag, index) in designTags" :key="index" class="flex">
-        <small class="light:text-[color:var(--grey)] light:border-[color:var(--grey)] rounded-4xl border border-solid px-5 py-2.5">{{ getTagName(tag) }}</small>
+  <a :href="designUrl" target="_blank" class="group light:bg-[var(--black)] design active relative z-10 grid h-125 gap-5 rounded-4xl p-10 hover:z-100 lg:items-center">
+    <h4 class="light:text-[color:var(--white)] flex gap-3">{{ designName }}</h4>
+    <p class="right-10 bottom-10 left-10 block self-end lg:absolute lg:hidden lg:group-hover:block">{{ designDesc }}</p>
+    <div class="right-10 bottom-10 left-10 flex flex-wrap gap-2 self-end lg:absolute lg:flex lg:group-hover:hidden">
+      <div v-for="(tag, index) in designTags" :key="index" class="flex light:text-[color:var(--grey)] light:border-[color:var(--grey)] rounded-4xl border border-solid px-5 py-2.5">
+        <small class="">
+          <span v-if="adaptiveLanding" class="icon-[fa7-solid--mobile-screen]"></span>
+          <span v-if="desktopLanding" class="icon-[fa7-solid--display]"></span>
+          {{ getTagName(tag) }}
+
+        </small>
       </div>
     </div>
   </a>
 </template>
 
 <script setup>
+import { computed } from "vue";
+
 const props = defineProps({
   designUrl: {
     type: String,
@@ -29,6 +36,9 @@ const props = defineProps({
     default: () => [],
   },
 });
+
+const adaptiveLanding = computed(() => props.designTags.some((tag) => (getTagName(tag) || "").toLowerCase().includes('adaptive landing')));
+const desktopLanding = computed(() => props.designTags.some((tag) => (getTagName(tag) || "").toLowerCase().includes('desktop landing')));
 
 function getTagName(tag) {
   if (tag == null) return "";
