15f1a9e8e53016444b00ca181f8898126372f2dd
d1eruki <stapps90@gmail.com>
2025-10-05 23:49:27 +0300

    optimizing tags for designs.vue

--- src/js/components/designs.vue
+++ src/js/components/designs.vue
@@ -1,12 +1,12 @@
 <template>
-  <a :href="designUrl" target="_blank" class="group light:bg-[var(--black)] design active z-10 row-span-1 grid h-full flex-col justify-center gap-5 rounded-4xl p-10 hover:z-100 hover:items-start hover:justify-between lg:min-h-[400px] lg:auto-rows-fr lg:items-center">
-    <h4 class="light:text-[color:var(--white)] self-end">{{ designName }}</h4>
-    <div class="flex flex-wrap gap-2 self-end lg:flex lg:group-hover:hidden">
+  <a :href="designUrl" target="_blank" class="group relative light:bg-[var(--black)] design active z-10 row-span-1 grid h-full flex-col justify-center gap-5 rounded-4xl p-10 hover:z-100 hover:items-start hover:justify-between lg:min-h-[400px] lg:items-center">
+    <h4 class="light:text-[color:var(--white)]">{{ designName }}</h4>
+    <p class="block self-end lg:hidden lg:group-hover:block">{{ designDesc }}</p>
+    <div class="absolute left-10 right-10 flex flex-wrap gap-2 self-end lg:flex lg:group-hover:hidden">
       <div v-for="(tag, index) in designTags" :key="index" class="flex">
-        <small class="light:text-[color:var(--grey-dark)] light:border-[color:var(--grey-dark)] rounded-4xl border border-solid px-5 py-2.5">{{ getTagName(tag) }}</small>
+        <small class="light:text-[color:var(--grey)] light:border-[color:var(--grey)] rounded-4xl border border-solid px-5 py-2.5">{{ getTagName(tag) }}</small>
       </div>
     </div>
-    <p class="block self-end lg:hidden lg:group-hover:block">{{ designDesc }}</p>
   </a>
 </template>
 
