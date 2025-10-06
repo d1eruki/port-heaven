d2e25c60915bd259f2fc754dbf2c00ea79a6a101
d1eruki <stapps90@gmail.com>
2025-10-06 22:08:39 +0300

    Kind of final fixes for header

--- src/js/components/designs.vue
+++ src/js/components/designs.vue
@@ -1,7 +1,7 @@
 <template>
-  <a :href="designUrl" target="_blank" class="group design relative z-10 grid lg:h-150 gap-5 rounded-4xl p-10 hover:z-100 lg:items-center">
+  <a :href="designUrl" target="_blank" class="group design relative z-10 grid gap-5 rounded-4xl p-10 hover:z-100 lg:h-150 lg:items-center">
     <h4 class="light:text-[color:var(--white)] flex gap-3">{{ designName }}</h4>
-    <div class="top-75 right-10 left-10 bottom-10 flex flex-wrap gap-5 self-end lg:absolute lg:flex lg:group-hover:hidden">
+    <div class="top-75 right-10 bottom-10 left-10 flex flex-wrap gap-5 self-end lg:absolute lg:flex lg:group-hover:hidden">
       <div v-for="(tag, index) in designTags" :key="index" class="light:text-[color:var(--grey)] flex gap-2.5">
         <small class="flex gap-2.5">
           <!--          <span v-if="isAdaptiveLanding(tag)" class="icon-[fa7-solid&#45;&#45;mobile-screen]"></span>
