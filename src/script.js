84e42c28f2107be9f076a040d90df5e079901afe
d1eruki <stapps90@gmail.com>
2025-10-06 22:00:41 +0300

    Disabling lang and scroll button

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 import "./style.scss";
 import "./js/vue";
 import "./js/scroll";
-import "./js/scroll-to-top";
+//import "./js/scroll-to-top";
 import "./js/theme-toggle";
 import "./js/locale-toggler";
 import "./js/yandex-metrika";
