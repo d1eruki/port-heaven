2f4487fb44480362a8a419873a37f9c05b2317f3
d1eruki <stapps90@gmail.com>
2025-10-07 00:57:15 +0300

    Small changes for cards rotating

--- src/js/card-rotate.js
+++ src/js/card-rotate.js
@@ -5,14 +5,19 @@ document.addEventListener("DOMContentLoaded", () => {
   if (cards.length) {
     cards.forEach((card) => {
       VanillaTilt.init(card, {
-        speed: 1000,
+        reverse: false,
         max: 10,
+        speed: 800,
+        perspective: 800,
         easing: "cubic-bezier(0.23, 1, 0.32, 1)",
-        transition: true,
         glare: true,
-        "max-glare": 0.35,
+        "max-glare": 0.3,
+        "glare-prerender": false,
+        gyroscope: true,
+        reset: true,
+        "reset-to-start": true,
+        transition: true,
       });
-
       card.addEventListener("mouseleave", () => {
         card.style.transition = "transform 0.8s cubic-bezier(0.23, 1, 0.32, 1)";
         card.style.transform = "perspective(800px) rotateX(0deg) rotateY(0deg)";
