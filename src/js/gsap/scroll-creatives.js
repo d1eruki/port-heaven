85178dcad11de3669f6dd0fe967df72a8437a9e6
d1eruki <stapps90@gmail.com>
2025-10-06 14:18:32 +0300

    Fix errors

--- src/js/gsap/scroll-creatives.js
+++ src/js/gsap/scroll-creatives.js
@@ -1,12 +1,20 @@
-let skewSetter = gsap.quickTo("img", "skewY"), // fast
-  clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
+import { gsap } from "gsap";
+import ScrollSmoother from "gsap/ScrollSmoother";
 
-ScrollSmoother.create({
-  wrapper: "#wrapper",
-  content: "#content",
-  smooth: 2,
-  speed: 3,
-  effects: true,
-  onUpdate: (self) => skewSetter(clamp(self.getVelocity() / -50)),
-  onStop: () => skewSetter(0),
+gsap.registerPlugin(ScrollSmoother);
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Set up a fast setter for skew and clamp the value to avoid extreme angles
+  const skewSetter = gsap.quickTo("img", "skewY");
+  const clamp = gsap.utils.clamp(-20, 20);
+
+  ScrollSmoother.create({
+    wrapper: "#wrapper",
+    content: "#content",
+    smooth: 2,
+    speed: 3,
+    effects: true,
+    onUpdate: (self) => skewSetter(clamp(self.getVelocity() / -50)),
+    onStop: () => skewSetter(0),
+  });
 });
