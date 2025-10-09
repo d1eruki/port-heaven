d86646fdc61939b94772aae0dc96657fbd22c550
d1eruki <stapps90@gmail.com>
2025-10-10 01:27:30 +0300

    Initial commit, cleaning trash

--- src/js/components/creatives.vue
+++ src/js/components/creatives.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="card-rotate vanilla-tilt h-fit" :style="computedStyle">
     <img class="card-rotate-face backface-hidden" :src="creativeSrc" alt="post-card" />
-    <div class="card-rotate-shirt light:bg-(--color-black) absolute top-0 h-full w-full backface-hidden">
-      <fieldset class="absolute top-0 w-full">
-        <legend>вредина</legend>
-      </fieldset>
-      <fieldset class="absolute top-[100%] w-full origin-top-right rotate-90">
-        <legend>негодяй</legend>
-      </fieldset>
-      <fieldset class="absolute bottom-0 w-full rotate-180">
-        <legend>злюка</legend>
-      </fieldset>
-      <fieldset class="absolute top-[100%] w-full origin-top-left -rotate-90">
-        <legend>лиходей</legend>
-      </fieldset>
-    </div>
+    <div class="card-rotate-shirt light:bg-(--color-black) absolute top-0 h-full w-full backface-hidden">asd</div>
   </div>
 </template>
 
