4c25a9538f913a57d153fbc32b3b8b1371d65605
d1eruki <stapps90@gmail.com>
2025-10-17 00:48:24 +0300

    removing gsap

--- src/script.js
+++ src/script.js
@@ -1,20 +1,13 @@
 import "./style.css";
 import "./js/vue";
 import "./js/yandex-metrika";
-
 import "./js/theme-toggle";
 import "./js/locale-toggler";
 import "./js/card-rotate";
 import "./js/cursor";
-
 import "./js/scroll-section";
+import "./js/lenis";
 
 import { updateProgressBar } from "./js/progress-bar";
-
-const LG_BREAKPOINT = 1024;
-
-if (window.innerWidth >= LG_BREAKPOINT) {
-  import("./js/gsap").catch(() => {});
-}
 
 window.onscroll = updateProgressBar;
