fdf6843ef5758dc6dd33ac09b1f975c883a94915
d1eruki <stapps90@gmail.com>
2025-10-28 19:31:42 +0300

    Refactor light theme styles: update hover effects, adjust color palette, and simplify design component structure.

--- src/js/vue-components/designs.vue
+++ src/js/vue-components/designs.vue
@@ -1,7 +1,7 @@
 <template>
-  <a :href="designUrl" target="_blank" class="design">
-    <div class="anim-extrude group relative z-10 grid gap-5 p-15 hover:z-50 lg:h-[50dvh] lg:items-center">
-      <h4 class="light:text-(--color-white) group-hover:light:text-(--color-black) flex gap-3">
+  <a :href="designUrl" target="_blank" class="light:hover:bg-(--color-yellow) light:text-(--color-white) light:hover:text-(--color-black)">
+    <div class="anim-extrude relative z-10 grid gap-5 p-15 hover:z-50 lg:h-[50dvh] lg:items-center">
+      <h4 class="flex gap-3">
         {{ designName }}
       </h4>
       <div class="top-75 right-15 bottom-15 left-15 flex flex-wrap gap-5 self-end lg:absolute lg:flex lg:group-hover:hidden">
