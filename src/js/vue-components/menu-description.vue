a26024c6f8ef7e19f8d057ef06a1d2b69792daf8
d1eruki <stapps90@gmail.com>
2025-10-28 21:03:22 +0300

    Update locales, refine styles, and add new hero media asset.

--- src/js/vue-components/menu-description.vue
+++ src/js/vue-components/menu-description.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="light:text-(--color-grey) w-full lg:max-w-150">{{ menuDesc }}</p>
+  <p class="light:text-(--color-grey) w-full">{{ menuDesc }}</p>
 </template>
 
 <script setup>
