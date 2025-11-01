ccfce7cb993cf58240aaf41dc01812486de78f4a
d1eruki <stapps90@gmail.com>
2025-11-01 19:09:32 +0300

    Update `features.vue` and `index.html`: refine layout and styling for improved responsiveness, replace "what" section with updated "numbers" section, and revise locale content accordingly.

--- src/js/vue-components/features.vue
+++ src/js/vue-components/features.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="light:border-neutral-900 light:lg:border-neutral-800 relative flex min-h-dvh flex-1 flex-col justify-between p-15 lg:min-h-full lg:gap-10 lg:border-l-1 lg:px-15 lg:py-0">
-    <span class="light:text-neutral-900 light:lg:text-neutral-800 absolute -bottom-12 -left-36 z-1 text-[24rem] font-black lg:static lg:text-9xl lg:font-thin">{{ number }}</span>
-    <h3 class="light:text-white z-2">{{ title }}</h3>
+  <div class="light:border-neutral-900 light:lg:border-neutral-800 relative grid min-h-dvh flex-1 xl:auto-rows-[1fr] justify-between p-15 lg:min-h-full lg:gap-10 lg:border-l-1 lg:px-15 lg:py-0">
+    <small class="light:text-white z-2">/ {{ title }}</small>
+    <span class="light:text-neutral-700 light:lg:text-neutral-700 z-1 text-[8rem] font-black lg:static lg:text-9xl lg:font-bold">{{ number }}</span>
     <p class="z-2">{{ text }}</p>
   </div>
 </template>
