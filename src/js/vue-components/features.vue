5464be4c4bc0853acb82307c66e4bf80fe081a8f
d1eruki <stapps90@gmail.com>
2025-11-01 17:56:35 +0300

    Refine `features.vue` styling: adjust light mode text and border colors for `lg` screens and update font weight of large text elements.

--- src/js/vue-components/features.vue
+++ src/js/vue-components/features.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="light:border-neutral-900 relative flex min-h-dvh flex-1 flex-col justify-between p-15 lg:min-h-full lg:gap-10 lg:border-l-1 lg:px-15 lg:py-0">
-    <span class="light:text-neutral-900 absolute -bottom-12 -left-36 z-1 text-[24rem] font-black lg:static lg:text-9xl lg:font-extralight">{{ number }}</span>
+  <div class="light:border-neutral-900 light:lg:border-neutral-800 relative flex min-h-dvh flex-1 flex-col justify-between p-15 lg:min-h-full lg:gap-10 lg:border-l-1 lg:px-15 lg:py-0">
+    <span class="light:text-neutral-900 light:lg:text-neutral-800 absolute -bottom-12 -left-36 z-1 text-[24rem] font-black lg:static lg:text-9xl lg:font-thin">{{ number }}</span>
     <h3 class="light:text-white z-2">{{ title }}</h3>
     <p class="z-2">{{ text }}</p>
   </div>
