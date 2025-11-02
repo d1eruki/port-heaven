42ba53763d6fe10fabee105847004d31a5609a96
d1eruki <stapps90@gmail.com>
2025-11-02 15:25:40 +0300

    Refactor `designs.vue` and `index.html`: update z-index values for improved stacking context, add hover icon to design links, and enhance layout consistency.

--- src/js/vue-components/designs.vue
+++ src/js/vue-components/designs.vue
@@ -1,6 +1,6 @@
 <template>
-  <a :href="designUrl" target="_blank" class="light:hover:bg-primary group light:text-white light:hover:text-black lg:h-[50dvh]">
-    <div class="anim-extrude relative z-10 grid gap-5 p-15 hover:z-50 lg:h-full lg:grid-rows-[50%] lg:items-center">
+  <a :href="designUrl" target="_blank" class="light:hover:bg-primary group light:text-white light:hover:text-black lg:h-[50dvh] lg:hover:z-51">
+    <div class="anim-extrude relative z-10 grid gap-5 p-15 lg:h-full lg:grid-rows-[50%] lg:items-center">
       <h4 class="flex gap-3">
         {{ designName }}
       </h4>
@@ -8,6 +8,7 @@
         <Tags :tags="designTags" :show-icons="true" class="opacity-100 lg:group-hover:hidden lg:group-hover:opacity-0" />
         <p class="lg:hidden lg:opacity-0 lg:group-hover:block lg:group-hover:opacity-100">{{ designDesc }}</p>
       </div>
+      <span class="icon-[ic--baseline-arrow-outward] absolute top-5 right-7.5 scale-200 opacity-0 group-hover:opacity-100"></span>
     </div>
   </a>
 </template>
