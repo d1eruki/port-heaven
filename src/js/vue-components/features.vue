83a62f98304c649d9d2c68ffcce432f81c9e147a
d1eruki <stapps90@gmail.com>
2025-11-02 15:32:56 +0300

    Refactor `features.vue`: adjust `min-h-dvh` to `min-h-fit` for improved layout behavior.

--- src/js/vue-components/features.vue
+++ src/js/vue-components/features.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="light:border-neutral-800 relative z-2 grid min-h-dvh flex-1 justify-between gap-15 p-15 lg:min-h-full lg:gap-20 lg:border-l-1 lg:p-15 xl:[grid-template-rows:auto_1fr_auto]">
+  <div class="light:border-neutral-800 relative z-2 grid min-h-fit flex-1 justify-between gap-15 p-15 lg:min-h-full lg:gap-20 lg:border-l-1 lg:p-15 xl:[grid-template-rows:auto_1fr_auto]">
     <small class="light:text-white">{{ title }}</small>
     <span class="light:text-primary text-[8rem] font-black lg:static lg:text-9xl lg:font-bold">{{ number }}</span>
     <p>{{ text }}</p>
