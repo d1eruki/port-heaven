d122524f29a80673e0ab12535d5b7251ba8f172a
d1eruki <stapps90@gmail.com>
2025-11-04 17:23:54 +0300

    adding simple swiper for future

--- src/js/vue-components/projects.vue
+++ src/js/vue-components/projects.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="light:lg:bg-black top-0 left-0 col-span-1 flex min-h-dvh w-full flex-col justify-between gap-10 p-15 will-change-transform">
-    <div class="flex flex-col gap-5">
-      <h3 class="light:text-white">{{ projectName }}</h3>
-      <p class="light:text-neutral-500">{{ projectDescription }}</p>
-      <Tags :tags="projectTags" :show-icons="true" />
+  <div class="light:lg:bg-black">
+    <!--
+    <div class="swiper mySwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="../../assets/projects/varwin/cover.png" alt="project-1" />
+        </div>
+        <div class="swiper-slide">
+          <img src="../../assets/projects/varwin/screen-1.png" alt="project-2" />
+        </div>
+      </div>
+      <div class="swiper-pagination light:bg-white z-100 light:text-black w-fit px-2.5 py-1 absolute left-0 bottom-0"></div>
     </div>
-    <div class="flex flex-col gap-5">
-      <p v-for="(text, index) in projectText" :key="index" class="light:text-white">{{ text }}</p>
-    </div>
-    <div class="flex flex-wrap gap-5 lg:gap-10">
-      <a v-for="(link, index) in projectLinks" :key="index" class="anim-extrude light:bg-primary active flex w-fit px-5 py-2.5 lg:px-10 lg:py-5" :href="link.url" target="_blank">
-        <p class="light:text-black font-[Actay_Wide]">{{ link.name }}</p>
-      </a>
+-->
+    <div class="flex min-h-dvh flex-col justify-between gap-10 p-15">
+      <div class="flex flex-col gap-5">
+        <h3 class="light:text-white">{{ projectName }}</h3>
+        <p class="light:text-neutral-500">{{ projectDescription }}</p>
+        <Tags :tags="projectTags" :show-icons="true" />
+      </div>
+      <div class="flex flex-col gap-5">
+        <p v-for="(text, index) in projectText" :key="index" class="light:text-white">{{ text }}</p>
+      </div>
+      <div class="flex flex-wrap gap-5 lg:gap-10">
+        <a v-for="(link, index) in projectLinks" :key="index" class="anim-extrude light:bg-primary active flex w-fit px-5 py-2.5 lg:px-10 lg:py-5" :href="link.url" target="_blank">
+          <p class="light:text-black font-[Actay_Wide]">{{ link.name }}</p>
+        </a>
+      </div>
     </div>
   </div>
 </template>
