e73c9ac9d9c92f3b30f9350f78b09789243b8d53
dieruki <stapps90@gmail.com>
2026-05-15 02:16:33 +0300

    Fix counter suffix layout

--- src/js/vue-components/features.vue
+++ src/js/vue-components/features.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="light:border-neutral-800 relative z-2 grid min-h-fit flex-1 justify-between gap-15 p-15 lg:min-h-full lg:gap-20 lg:border-l-1 lg:p-15 xl:[grid-template-rows:auto_1fr_auto]">
     <small class="light:text-white">{{ title }}</small>
-    <span
-      :data-target="dataTarget"
-      :class="[
-        idAttr,
-        {
-          plus: typeof dataTarget === 'string' && dataTarget.includes('+'),
-        },
-      ]"
-      class="light:text-primary text-[8rem] font-black lg:static lg:text-9xl lg:font-bold"
-    >
-      {{ displayValue }}
+    <span class="light:text-primary inline-flex w-fit items-baseline gap-[0.08em] text-[8rem] font-black lg:text-9xl lg:font-bold">
+      <span
+        :data-target="dataTarget"
+        :class="idAttr"
+      >
+        {{ displayValue }}
+      </span>
+      <span v-if="suffix">{{ suffix }}</span>
     </span>
     <p>{{ text }}</p>
   </div>
@@ -36,4 +33,5 @@ const props = defineProps({
 });
 
 const displayValue = typeof props.dataTarget === "string" ? props.dataTarget.replace(/[^\d]/g, "") || props.dataTarget : props.dataTarget;
+const suffix = typeof props.dataTarget === "string" ? props.dataTarget.replace(/[\d\s]/g, "") : "";
 </script>
