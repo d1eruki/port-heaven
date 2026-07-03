dcd9f4f8a71cac512b6227b7132ebe8515ec7673
dieruki <stapps90@gmail.com>
2026-07-03 12:29:05 +0300

    Refactor webpack configuration to dynamically set mode and improve maintainability.

--- webpack.config.js
+++ webpack.config.js
@@ -6,77 +6,82 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-const isDev = process.env.NODE_ENV === "development";
+module.exports = (_env, argv) => {
+  const mode = argv.mode || process.env.NODE_ENV || "production";
+  const isDev = mode === "development";
 
-module.exports = {
-  entry: "./src/script.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: isDev ? "script.js" : "script.[contenthash].js",
-    chunkFilename: isDev ? "[name].js" : "[name].[contenthash].js",
-    clean: true,
-  },
-  mode: isDev ? "development" : "production",
-  devServer: {
-    static: path.join(__dirname, "dist"),
-    port: 8080,
-    open: false,
-    allowedHosts: "all",
-    hot: true,
-  },
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
+  return {
+    entry: "./src/script.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: isDev ? "script.js" : "script.[contenthash].js",
+      chunkFilename: isDev ? "[name].js" : "[name].[contenthash].js",
+      clean: true,
     },
-    extensions: [".js", ".vue", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-        generator: { filename: isDev ? "assets/[name][ext]" : "assets/[name].[contenthash][ext]" },
+    mode,
+    devServer: {
+      static: path.join(__dirname, "dist"),
+      port: 8080,
+      open: false,
+      allowedHosts: "all",
+      hot: true,
+    },
+    resolve: {
+      alias: {
+        vue: "vue/dist/vue.esm-bundler.js",
       },
-      {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: {
-                plugins: [require("@tailwindcss/postcss")],
+      extensions: [".js", ".vue", ".json"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: isDev ? "assets/[name][ext]" : "assets/[name].[contenthash][ext]",
+          },
+        },
+        {
+          test: /\.vue$/,
+          loader: "vue-loader",
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [require("@tailwindcss/postcss")],
+                },
               },
             },
-          },
-        ],
-      },
+          ],
+        },
+      ],
+    },
+    optimization: {
+      minimize: !isDev,
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+        filename: "index.html",
+        inject: true,
+      }),
+      ...(isDev ? [] : [new MiniCssExtractPlugin({ filename: "style.[contenthash].css" })]),
+      new CopyWebpackPlugin({
+        patterns: [{ from: "src/assets", to: "assets" }],
+      }),
+      new DefinePlugin({
+        __VUE_OPTIONS_API__: JSON.stringify(false),
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
     ],
-  },
-  optimization: {
-    minimize: !isDev,
-    minimizer: [`...`, new CssMinimizerPlugin()],
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
-      inject: true,
-    }),
-    ...(isDev ? [] : [new MiniCssExtractPlugin({ filename: "style.[contenthash].css" })]),
-    new CopyWebpackPlugin({
-      patterns: [{ from: "src/assets", to: "assets" }],
-    }),
-    new DefinePlugin({
-      __VUE_OPTIONS_API__: JSON.stringify(false),
-      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
-      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
-    }),
-  ],
+  };
 };
