834506cb4a5ada42b8965ae07cd295a3e4951eff
dieruki <stapps90@gmail.com>
2026-07-11 19:33:21 +0300

    Remove: Unused Vue alias from Webpack `resolve` configuration

--- webpack.config.js
+++ webpack.config.js
@@ -29,9 +29,6 @@ module.exports = (_env, argv) => {
       hot: true,
     },
     resolve: {
-      alias: {
-        vue: "vue/dist/vue.esm-bundler.js",
-      },
       extensions: [".js", ".vue", ".json"],
     },
     module: {
