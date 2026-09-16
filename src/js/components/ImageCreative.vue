a0be764ad051f9fdbf49eb1ddc5ca5d2517b688c
dieruki <stapps90@gmail.com>
2026-09-16 12:07:11 +0200

    Reorganize creative assets, refine class usage in components
    - Reorganized creative assets across priority folders to better align with updated structure and prioritization.
    - Removed unused `rounded-4xl` class from `ImageCreative.vue` and `VideoCreative.vue` templates.
    - Added new assets and cleaned up redundant files.

--- src/js/components/ImageCreative.vue
+++ src/js/components/ImageCreative.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="vanilla-tilt-creatives group h-fit overflow-hidden rounded-4xl hover:z-100"
+    class="vanilla-tilt-creatives group h-fit overflow-hidden hover:z-100"
   >
     <img
       class="h-auto w-full transition-all duration-1000 ease-in-out"
--- src/js/components/VideoCreative.vue
+++ src/js/components/VideoCreative.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="vanilla-tilt-creatives group h-fit overflow-hidden rounded-4xl hover:z-100"
+    class="vanilla-tilt-creatives group h-fit overflow-hidden hover:z-100"
   >
     <video
       ref="videoEl"
